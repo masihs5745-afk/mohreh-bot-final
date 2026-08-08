12f2c062411a3ae9514ba87d853681adcf2f3efd
masihs5745-afk <masihs5745@gmail.com>
2026-08-08 18:26:03 +0330

    Include my_orders router in main.py
    Added my_orders router to the main dispatcher.

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from bot.config import BOT_TOKEN
+
 from bot.handlers.start import router as start_router
 from bot.handlers.order import router as order_router
 from bot.handlers.support import router as support_router
 from bot.handlers.admin import router as admin_router
 from bot.handlers.admin_panel import router as admin_panel_router
 from bot.handlers.info import router as info_router
+from bot.handlers.my_orders import router as my_orders_router
 
 from bot.database.database import create_db
 from bot.database.support import create_support_table
@@ -25,6 +27,7 @@ dp.include_router(support_router)
 dp.include_router(admin_router)
 dp.include_router(admin_panel_router)
 dp.include_router(info_router)
+dp.include_router(my_orders_router)
 
 
 async def main():
