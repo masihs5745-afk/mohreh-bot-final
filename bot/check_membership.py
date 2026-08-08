442b23c2ba0af3b7256d3a896606ae3fe8b076b5
masihs5745-afk <masihs5745@gmail.com>
2026-08-08 17:13:25 +0330

    Update check_membership.py

--- bot/check_membership.py
+++ bot/check_membership.py
@@ -1,1 +1,22 @@
+from aiogram import Bot
 
+
+async def check_membership(
+    bot: Bot,
+    user_id: int,
+    channel: str,
+) -> bool:
+    try:
+        member = await bot.get_chat_member(
+            chat_id=channel,
+            user_id=user_id,
+        )
+
+        return member.status in {
+            "member",
+            "administrator",
+            "creator",
+        }
+
+    except Exception:
+        return False
