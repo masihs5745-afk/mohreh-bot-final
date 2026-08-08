57ba7a753a2d18a6495b90072b47fa7a7e0c76e2
masihs5745-afk <masihs5745@gmail.com>
2026-08-08 18:13:57 +0330

    Add CHANNEL_ID and CHANNEL_URL to config

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,12 @@
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
+
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+CHANNEL_URL = os.getenv("CHANNEL_URL")
